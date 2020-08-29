0c68d290fe80be0553f9eb1172c834d961cc1fe8
Shimkek <maksim.dunaitsev@gmail.com>
2020-08-30 02:24:22 +0600

    Added display of the search results

--- src/scripts.js
+++ src/scripts.js
@@ -41,7 +41,7 @@ const fillRepoList = (object) => {
     for (let i=0; i<object.items.length; i++) {
         const li = document.createElement('li');
         const full_name = object.items[i].full_name;
-        const watchers_count = object.items[i].watchers_count;
+        const watchers_count_text = object.items[i].watchers_count;
         const description = object.items[i].description;
         const language = object.items[i].language;
         const updated_at = object.items[i].updated_at;
@@ -49,9 +49,49 @@ const fillRepoList = (object) => {
 
         const full_name_El = document.createElement('a');
         full_name_El.textContent = full_name;
+        full_name_El.className = `repoList__name`;
         full_name_El.href = html_url;
+        
+        const description_El = document.createElement('p');
+        description_El.textContent = description;
+
+        const watchers_El = document.createElement('a');
+        const star = document.createElement('span');
+        star.style.fontSize='20px';
+        star.textContent = `☆`;
+        const watchers_number = document.createElement('span');
+        watchers_number.style.fontSize='14px';
+        watchers_number.textContent = `${watchers_count_text}`;
+        watchers_El.appendChild(star);
+        watchers_El.appendChild(watchers_number);
+        watchers_El.className = `repoList__watchersCount`;
+        watchers_El.href = `${html_url}/stargazers`;
+
+        const language_El = document.createElement('span');
+        language_El.style.fontSize='12px';
+        language_El.className=`repoList__language`;
+        language_El.textContent= `  ${language}  `;
+
+        const updated_at_El = document.createElement('span');
+        updated_at_El.style.fontSize='12px';
+        updated_at_El.textContent = `Updated on ${updated_at.slice(0, 10)}`;
+
+        
 
         li.appendChild(full_name_El);
+        li.appendChild(description_El);
+        li.appendChild(watchers_El);
+        li.appendChild(language_El);
+
+        if (object.items[i].license !== null) {
+            const license_El = document.createElement('span');
+            license_El.style.fontSize='12px';
+            license_El.textContent = `${object.items[i].license.name}`;
+            license_El.className = `repoList__license`;
+            li.appendChild(license_El);
+        };
+        
+        li.appendChild(updated_at_El);
         li.className = "repoList__item";
         ul.appendChild(li);
     }
